d5eb790188d73c35ad6bd8bee09357d2eb01224b
David Patrício <david.c.patricio@gmail.com>
2023-05-25 19:54:41 +0100

    Criar os templates necessários para o layout

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -4,8 +4,19 @@ from django.shortcuts import render
 def index(request):
     return render(
         request,
-        'blog/pages/index.html',
-        {
-            'nome': 'David Patrício'
-        }
+        'blog/pages/index.html'
+    )
+
+
+def page(request):
+    return render(
+        request,
+        'blog/pages/page.html'
+    )
+
+
+def post(request):
+    return render(
+        request,
+        'blog/pages/post.html'
     )
